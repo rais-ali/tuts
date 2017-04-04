fbbf732322e33b04add150c7fc46e8d295a49959
rais-ali <2014kucp1006@iiitkota.ac.in>
2017-04-04 23:45:25 +0530

    initial commit

---
+++ disjoint.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class disjoint {
+	public static void main(String srgs[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int m=sc.nextInt(),n=sc.nextInt();
+		HashSet<Integer> hash=new HashSet<Integer>(m);
+		
+		int flag=0;
+		for(int i=0;i<m;i++)
+			hash.add(sc.nextInt());
+		System.out.println("Enter elements of array 2");
+        for(int i=0;i<n;i++)
+		{
+			if(hash.contains(sc.nextInt()))
+			{
+                 flag=1;
+			}
+		}
+		if(flag==1)
+			System.out.println("Not Disjoint");
+		else
+			System.out.println(" Disjoint sets");
+	}
+}
